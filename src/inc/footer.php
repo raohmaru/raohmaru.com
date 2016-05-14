a660d14685640d0ac35506f9e484718810047f2c
raohmaru <namoracon@gmail.com>
2016-05-15 00:36:06 +0200

    Fotter: Removed RSS button, added Github link

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -3,8 +3,8 @@
 			<li class="footer-sep">© 2016<span id="footer-copy"> Raúl Parralejo</span></li>
 			<li id="footer-work" class="footer-sep">Front-end developer</li>
 			<li class="group-icons"><a href="/blog" class="sprite icon-blog" title="Blog">Blog</a></li>
-			<li class="group-icons"><a href="/blog/feed/" class="sprite icon-rss" title="RSS">RSS</a></li>
 			<li class="group-icons"><a href="http://es.linkedin.com/in/raulpn" target="_blank" class="sprite icon-linkedin" title="Perfil en LinkedIn" rel="me external">LinkedIn</a></li>
 			<li class="group-icons"><a href="http://twitter.com/raulparralejo" target="_blank" class="sprite icon-twitter" title="Sígueme en Twitter" rel="external">Twitter</a></li>
+			<li class="group-icons"><a href="https://github.com/raohmaru" target="_blank" class="sprite icon-github" title="Mis repositorios en GitHub">GitHub</a></li>
 		</ul>
 	</footer>
