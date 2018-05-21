ae306ff2cb6de70f74c48a4343a83b79b974b7be
raohmaru <namoracon@gmail.com>
2018-05-21 18:21:05 +0200

    Footer links to https version

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -2,9 +2,9 @@
 		<ul>
 			<li class="footer-sep">© 2016<span id="footer-copy"> Raúl Parralejo</span></li>
 			<li id="footer-work" class="footer-sep">Front-end developer</li>
-			<li class="group-icons"><a href="/blog" class="sprite icon-blog" title="Blog">Blog</a></li>
-			<li class="group-icons"><a href="http://es.linkedin.com/in/raulpn" target="_blank" class="sprite icon-linkedin" title="Perfil en LinkedIn" rel="me external">LinkedIn</a></li>
-			<li class="group-icons"><a href="http://twitter.com/raulparralejo" target="_blank" class="sprite icon-twitter" title="Sígueme en Twitter" rel="external">Twitter</a></li>
+			<li class="group-icons"><a href="https://raohmaru.com/blog" class="sprite icon-blog" title="Blog">Blog</a></li>
+			<li class="group-icons"><a href="https://es.linkedin.com/in/raulpn" target="_blank" class="sprite icon-linkedin" title="Perfil en LinkedIn" rel="me external">LinkedIn</a></li>
+			<li class="group-icons"><a href="https://twitter.com/raulparralejo" target="_blank" class="sprite icon-twitter" title="Sígueme en Twitter" rel="external">Twitter</a></li>
 			<li class="group-icons"><a href="https://github.com/raohmaru" target="_blank" class="sprite icon-github" title="Mis repositorios en GitHub">GitHub</a></li>
 		</ul>
 	</footer>
